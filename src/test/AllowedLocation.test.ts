e337252cf34a4b66cd334023b2b21d4713b964ea
Johanna Ahlskog <johanna.ahlskog@omegapoint.com>
2023-09-11 10:14:47 +0200

    Fixed tests to azure controls

---
+++ src/test/AllowedLocation.test.ts
@@ -0,0 +1,30 @@
+import chai, { expect } from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import { isLocationAllowed } from '../allowedlocation/lib/AllowedLocations';
+
+chai.use(sinonChai);
+
+describe('Allowed Location Test', () => {
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  it('Should return true if location is within allowed locations', () => {
+    const isEnabled: boolean = isLocationAllowed(['europe']);
+    expect(isEnabled).equal(true);
+  });
+  it('Should throw error if location is empty', () =>
+    expect(function () {
+      isLocationAllowed([]);
+    }).to.throw('Locations must contain values'));
+  it('Should throw error if location is undefined', () =>
+    expect(function () {
+      isLocationAllowed(undefined);
+    }).to.throw('Locations must contain values'));
+
+  it('Should throw error if location is null', () =>
+    expect(function () {
+      isLocationAllowed(null);
+    }).to.throw('Locations must contain values'));
+});
---
+++ src/test/TestData.ts
@@ -0,0 +1,26 @@
+import { InfoPolicyType } from '../allowedlocation/lib/types/InfoPolicyType';
+
+const name: string = 'testPolicy';
+const subscriptionId: string = 'testSubscriptionId';
+const managementGroup: string = 'testmanagementGroup';
+const allowedLocations: string[] = ['sweden', 'europe'];
+const enforcementModeOn: string = 'Enabled';
+const enforcementModeOff: string = 'DoNotEnforce';
+
+export const policyEnabled: InfoPolicyType = {
+  name: name,
+  subscriptionId: subscriptionId,
+  managementGroup: managementGroup,
+  allowedLocations: allowedLocations,
+  enforcementMode: enforcementModeOn,
+};
+
+export const policyNotEnabled: InfoPolicyType = {
+  name: name,
+  subscriptionId: subscriptionId,
+  managementGroup: managementGroup,
+  allowedLocations: allowedLocations,
+  enforcementMode: enforcementModeOff,
+};
+
+export const PoliciyTypes: InfoPolicyType[] = [policyEnabled, policyNotEnabled];
