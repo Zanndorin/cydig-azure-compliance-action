23ad42070dce58215fc59672f08aa2b67a86c921
Jonathan Bokvad Engarås <jonathan.bokvad.engaras@omegapoint.se>
2023-10-10 13:07:08 +0200

    refactor project

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,7 @@ export async function run(): Promise<void> {
     if (cydigConfig.usingAzure) {
       const credentials: DefaultAzureCredential = new DefaultAzureCredential();
       const subscriptionId: string = core.getInput('subscription');
+      if (!subscriptionId) throw new Error('Could not get subscriptionId');
       await SecureScore.getSecureScore(credentials, subscriptionId);
       await DeployedVirtualMachines.getDeployedVirtualMachines(credentials, subscriptionId);
       await AllowedLocation.getAllowedLocation(credentials, subscriptionId);
