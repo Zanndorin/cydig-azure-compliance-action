5436a70798fcabcd6b6da759a115cef5ba09c520
Linnéa Oxenwaldt <linnea.oxenwaldt@basalt.se>
2024-02-28 08:18:45 +0100

    delete validation och schema

--- src/types/CyDigConfig.ts
+++ src/types/CyDigConfig.ts
@@ -3,8 +3,21 @@ export type CyDigConfig = {
   usingAzure: boolean;
   threatModeling: {
     date: string;
+    boardsTag: string;
   };
   pentest: {
     date: string;
+    boardsTag: string;
   };
+  azureDevOps: {
+    usingBoards: boolean
+    boards: {
+      organizationName: string;
+      projectName: string;
+      nameOfBoard: string;
+    }
+  }
+  scaTool: string;
+  sastTool: string;
+  codeQualityTool: string;
 };
