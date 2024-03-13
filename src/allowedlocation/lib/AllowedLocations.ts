0240b9a1311d6ead56438ca152e22dc88a866ed2
Linnéa Oxenwaldt <linnea.oxenwaldt@basalt.se>
2024-03-13 10:01:13 +0100

    lint and prettier

--- src/allowedlocation/lib/AllowedLocations.ts
+++ src/allowedlocation/lib/AllowedLocations.ts
@@ -21,7 +21,7 @@ export function isLocationAllowed(location: string[]): boolean {
     throw Error('Locations must contain values');
   }
   let policyValid: boolean = true
-  for (let index = 0; index < location.length; index++) {
+  for (let index: number = 0; index < location.length; index++) {
     if(!allowedLocations.includes(location[index])) {
       console.log(`${location[index]} is not in the list of allowed locations. Contact the CyDig team if the location is in Europe`)
       policyValid = false
